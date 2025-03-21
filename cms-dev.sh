144e82d393561a6a7ef8c7baa2d3635f99a8fd05
Hugo Peyraud-Magnin <hugo.pm@pm.me>
2025-03-21 15:00:25 +0100

    hardcode docker container names

--- cms-dev.sh
+++ cms-dev.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-
-docker compose -p $GIT_BRANCH_NAME -f docker-compose.dev.yml run  --build --rm --service-ports devcms
+docker compose -p cmsdev -f docker-compose.dev.yml run  --build --rm --service-ports devcms
--- cms-test.sh
+++ cms-test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-
-docker compose -p $GIT_BRANCH_NAME -f docker-compose.test.yml run --build --rm testcms
+docker compose -p cmstest -f docker-compose.test.yml run --build --rm testcms
